d6023f056a7295dfb2ab3ab466b0029c3f3d8d49
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-03-27 00:37:48 +0530

    add a middleware and function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from '../user.model.js';
 import bcryptjs from 'bcryptjs';
-export const signup= async(req,res)=>{
+import { errorHandler } from '../utils/error.js';
+export const signup= async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username || !email || !password || username==='' || email==='' || password===''){
-        return res.status(400).json({message:"All fields are required"});
+        next(errorHandler(400,"All Feilds are required!"));
     }
     const hashedPassword=bcryptjs.hashSync(password,10);
     const newUser =new User({
@@ -11,8 +12,8 @@ export const signup= async(req,res)=>{
     })
     try {
         await newUser.save();
-    res.json("signup successful");
+        res.json("signup successful");
     } catch (error) {
-        res.status(500).json({message:error.message});
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-
+  
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -21,6 +21,15 @@ app.use(express.json());
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
+})
 
 app.listen(3000,()=>{
     console.log(`Listining on port 3000 `);
