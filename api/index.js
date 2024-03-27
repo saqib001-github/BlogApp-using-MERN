cc648c3e4f986dfd637dd2c440e7f7798658ddd3
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-03-27 22:15:34 +0530

    add functionality to SignUp page

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-  
-dotenv.config();
 
-mongoose.connect(process.env.MONGO)
+dotenv.config();
+  
+mongoose.connect(process.env.MONGO)  
 .then(
     ()=>{
         console.log("Mongo DB is connected.");
     }
 ).catch((err)=>{
-    console.log(err);
+    console.error('MongoDB connection error:', err);
 })
 
 const app=express();
