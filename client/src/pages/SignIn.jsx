436144e7602b32a9641fab089becc5d5a4852ec2
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-03-30 22:22:52 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,16 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
 import { useState } from "react";
 import { Link , useNavigate} from "react-router-dom"
+import {useDispatch,useSelector} from 'react-redux';
+import {signInStart,signInFailure,signInSuccess} from '../redux/user/userSlice.js';
+
+
 export default function SignIn() {
+  
   const navigate=useNavigate();
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading ,error:errorMessage}=useSelector(state=>state.user);
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
 
@@ -13,11 +18,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => { 
     e.preventDefault();
     if(!formData.email || !formData.password){
-      return setErrorMessage("All fields are required!");
+      return dispatch(signInFailure("All fields are required!"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/sign-in', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
@@ -26,19 +30,14 @@ export default function SignIn() {
       
       const data = await res.json();
       if(data.success===false){
-        setLoading(false)
-        return setErrorMessage(data.message);
+        return dispatch(signInFailure(data.message));
       }
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate('/');
       }
-      setLoading(false);
-
-
-      
     } catch (error) {
-      console.log('Fetch error',error);
-      setLoading(false)
+      dispatch(signInFailure(error.message));
     }
   }
   return (
