84b50e90060b55164559f40120a6228183dadae5
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-05-29 13:45:39 +0530

    added delete post functionality to dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,12 +1,15 @@
-import { Table } from 'flowbite-react';
+import { Table,Modal, Button } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showModal,setShowModal]=useState(false);
+  const [postToDelete,setPostToDelete] = useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -41,6 +44,25 @@ export default function DashPosts() {
       }
     } catch (error) {
       console.log(error);
+    }
+  }
+  const handleDeletePost = async ()=>{
+    setShowModal(false)
+    try{
+      const res=await fetch(`/api/post/deleteposts/${postToDelete}/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        setUserPosts(prev => 
+          prev.filter(post => post._id !== postToDelete )
+        );
+      }
+
+    }catch(error){
+      console.log(error.message);
     }
   }
 
@@ -84,7 +106,7 @@ export default function DashPosts() {
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span
+                    <span onClick={()=> {setShowModal(true); setPostToDelete(post._id)}}
                       className='font-medium text-red-500 hover:underline cursor-pointer'
                     >
                       Delete
@@ -109,6 +131,19 @@ export default function DashPosts() {
       ) : (
         <p>You have no posts yet!</p>
       )}
+      <Modal show={showModal} size={`md`} onClose={()=>setShowModal(false)}>
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>Are you sure you want to delete this post ?</h3>
+            <div className='flex justify-center gap-6'>
+              <Button color='success' onClick={handleDeletePost} >{"Yes"}</Button>
+              <Button color='failure' onClick={()=> setShowModal(false)} >{"No,Cancel"}</Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
