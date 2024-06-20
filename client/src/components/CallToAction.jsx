fc2a7fc6a3cac22b3ee4a10ee7461f7be7ef0634
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-06-20 18:33:39 +0530

    add call to action to post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,22 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className='flex-1 justify-center flex flex-col'>
+            <h2 className='text-2xl'>Want to learn more about JavaScript?</h2>
+            <p className='text-gray-500 my-2'>Checkout these resources.</p>
+            <Button gradientDuoTone={'purpleToPink'} className='rounded-tl-xl rounded-bl-none '>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Click Me</a>
+            </Button>
+        </div>
+        <div className='p-7'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk0IaRDjBvZC4Ga4GfQPWxacgvFVTF1rlWUw&s" />
+        </div>
+
+    </div>
+  )
+}
+
+export default CallToAction
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { Button, Spinner } from "flowbite-react";
+import CallToAction from "../components/CallToAction";
 const PostPage = () => {
     const {postSlug} = useParams();
     const [loading,setLoading] = useState(true);
@@ -43,7 +44,10 @@ const PostPage = () => {
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
             <span className=" italic">{post && (post.content.length /1000).toFixed(0)} mins read</span>
         </div>
-        <div className="p-3 max-w-2xl mx-auto w-full postcontent" dangerouslySetInnerHTML={{__html:post && post.content}}></div>
+        <div className="p-3 max-w-2xl mx-auto w-full postcontent" dangerouslySetInnerHTML={{__html:post && post.content}}>
+
+        </div>
+        <div className="max-w-4xl mx-auto w-full "><CallToAction/></div>
     </main>
   )
 }
