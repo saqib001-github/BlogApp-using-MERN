ab98bf18222c14029cd4bdd4947be31175958923
saqib1202 <132768429+saqib1202@users.noreply.github.com>
2024-07-18 00:46:23 +0530

    added pre installition

--- api/index.js
+++ api/index.js
@@ -4,40 +4,49 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js'
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-dotenv.config(); 
+import path from 'path';
 
-try { 
+dotenv.config();
 
-    await mongoose.connect(process.env.MONGO);
-    console.log("Mongo DB is connected.");
-} catch (err) {
-    console.error('MongoDB connection error:', err);
-}
- 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
-})
-
-app.listen(3000, () => {
-    console.log(`Listining on port 3000 `);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
- 
